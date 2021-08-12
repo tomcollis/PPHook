0ae9a4972ed206a1b75d0112ebde67c45559e4e8
Tom Collis <tom.collis@gmail.com>
2021-08-12 12:56:03 +0100

    Fix to implement API KEY from Environment Variables.

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use('/', function(req, res, next){
   // key isn't present
   if (!key) return next(error(400, 'api key required'));
   // key is invalid
-  if (!~apiKeys.indexOf(key)) return next(error(401, 'invalid api key'));
+  if (key !== apiKeys) return next(error(401, 'invalid api key'));
   // all good, store req.key for route access
   req.key = key;
   next();
